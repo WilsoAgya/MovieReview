394500163407fdd4759118ce1ecd29ce2ed59839
WilsoAgya <wilsonagyapong16@gmail.com>
2023-05-30 18:05:40 -0700

    Made API Key

--- js/movies.js
+++ js/movies.js
@@ -1,10 +1,13 @@
+import {apiKey} from './config.js';
+
+
 async function myFunction(){
 
 const url = 'https://movie-database-alternative.p.rapidapi.com/?s=Star%20Wars&r=json&page=1';
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '3db8a0f822msh8ce3b9ea70d3873p19afedjsn6ac73d0e6a7d',
+		'X-RapidAPI-Key' : apiKey,
 		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
 	}
 };
