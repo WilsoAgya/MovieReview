3777938d7c32e10a365597ff9bc43bafd98a2383
WilsoAgya <wilsonagyapong16@gmail.com>
2023-05-31 16:18:07 -0700

    Made popup modal work when adding frames

--- js/searchmovie.js
+++ js/searchmovie.js
@@ -1,4 +1,44 @@
-var modal = document.getElementById("movielist");
+
+document.getElementById("addButton").addEventListener("click", function() {
+    document.getElementById("movielist").style.display = "block";
+  });
+  
+  document.getElementsByClassName("close")[0].addEventListener("click", function() {
+    document.getElementById("movielist").style.display = "none";
+  });
+  
+  document.getElementById("movielist").addEventListener("click", function(event) {
+    if (event.target === this) {
+      document.getElementById("movielist").style.display = "none";
+    }
+  });
+  
+  document.getElementById("addButton").addEventListener("click", function() {
+    var newButton = document.createElement("button");
+    newButton.addEventListener("click", function() {
+      var addButton = document.createElement("button");
+      document.getElementById("buttoncontainer").appendChild(addButton);
+    });
+    document.getElementById("buttoncontainer").appendChild(newButton);
+  });
+  
+
+
+
+
+
+
+/*document.getElementById("addButton").addEventListener("click", function() {
+    document.getElementById("movielist").style.display = "block";
+
+});
+
+document.getElementsByClassName("close")[0].addEventListener("click",function(){
+    document.getElementById("movielist").style.display="none";
+});*/
+
+
+/*var modal = document.getElementById("movielist");
 var closeButton = document.getElementById("close-modal");
 
 function openList(){
@@ -15,4 +55,4 @@ window.addEventListener("click", function(event) {
     if(event.target == modal){
         closeList();
     }
-});
+});*/
